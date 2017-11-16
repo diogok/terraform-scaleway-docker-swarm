b1b94e8a3277ae30235f17cc376e29732dea3048
Diogo Silva <diogo@diogok.net>
2017-11-16 01:06:06 -0200

    allow insecure registry from swarm_manager

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -28,11 +28,19 @@ sudo apt-get update
 sudo apt-get install docker-ce -y
 
 ROLE=$(cat /tmp/role)
+MANAGER_IP=$(cat /tmp/swarm_manager)
 if [ "$ROLE" == "manager" ]; then
   echo "Setting up TLS on manager"
-  OPTS='-H0.0.0.0:2376 -H fd:\/\/ --tlsverify --tlscacert=\/opt\/keys\/ca.pem --tlscert=\/opt\/keys\/server-cert.pem --tlskey=\/opt\/keys\/server-key.pem'
+  OPTS="-H0.0.0.0:2376 -H fd:\/\/ --tlsverify --tlscacert=\/opt\/keys\/ca.pem --tlscert=\/opt\/keys\/server-cert.pem --tlskey=\/opt\/keys\/server-key.pem --insecure-registry $MANAGER_IP:5000"
   sed -i -e "s/-H fd:\/\//$OPTS/" /lib/systemd/system/docker.service
   sudo systemctl daemon-reload
   sudo systemctl restart docker
 fi
 
+if [ "$ROLE" == "worker" ]; then
+  echo "Setting up TLS on manager"
+  OPTS="-H fd:\/\/ --insecure-registry $MANAGER_IP:5000"
+  sed -i -e "s/-H fd:\/\//$OPTS/" /lib/systemd/system/docker.service
+  sudo systemctl daemon-reload
+  sudo systemctl restart docker
+fi
